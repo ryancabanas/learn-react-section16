9d1f63da02b3abf2ee50b5f3d0df6d7b155a2b6d
Ryan Cabanas <ryangithub777@gmail.com>
2022-07-21 21:24:58 -0400

    End of Lesson 200: Obtain an input with `useRef` instead of the `onChange` event.
    - We remove the `state` and the change event handler and we use `useRef` instead.
    
    - With this approach, we can't do dynamic input validation, but as you can see,
      it's less code.  You create a `ref` with `useRef` and you bind it to the
      input with the `ref` attribute.  Then you just reference that `ref` during
      the form submission handler call.
    
    - Another thing you can't (or at least shouldn't) do with a `ref` is clear out
      the input field after form submission.  When you do it, like seen in these
      changes, you're actually manipulating the DOM and this is a bad practice.  You
      should allow only React to modify the DOM.

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,28 +1,19 @@
-import { useState } from 'react';
+import { useRef } from 'react';
 
 const SimpleInput = (props) => {
-  const [enteredName, setEnteredName] = useState('');
-
-  const nameInputChangeHanlder = (event) => {
-    setEnteredName(event.target.value);
-  };
+  const nameInputRef = useRef();
 
   const formSubmissionHandler = (event) => {
     event.preventDefault();
-    console.log(enteredName);
-    setEnteredName('');
+    console.log(nameInputRef.current.value);
+    nameInputRef.current.value = '';
   };
 
   return (
     <form onSubmit={formSubmissionHandler}>
       <div className="form-control">
         <label htmlFor="name">Your Name</label>
-        <input
-          type="text"
-          id="name"
-          onChange={nameInputChangeHanlder}
-          value={enteredName}
-        />
+        <input type="text" id="name" ref={nameInputRef} />
       </div>
       <div className="form-actions">
         <button>Submit</button>
