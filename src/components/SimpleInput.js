2298f9d41c19b9ec120db27f645547868f31016a
Ryan Cabanas <ryangithub777@gmail.com>
2022-07-21 21:18:37 -0400

    Lesson 200: Use the `onChange` event with a handler function and a `state` variable, to gather the value from an input.
    - You have an input handler function and a form submit handler function.  The
      input is dynamically captured this way with the `onChange` event, while the
      submit form handler uses that input value.
    
      Don't forget the call the `event.preventDefault` function in the submit form
      handler!
    
    - If you want to clear out the input after form submission, use the `setState`
      function to set the `state` back to an empty value and then bind it to the
      input using the `value` attribute.

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,9 +1,28 @@
+import { useState } from 'react';
+
 const SimpleInput = (props) => {
+  const [enteredName, setEnteredName] = useState('');
+
+  const nameInputChangeHanlder = (event) => {
+    setEnteredName(event.target.value);
+  };
+
+  const formSubmissionHandler = (event) => {
+    event.preventDefault();
+    console.log(enteredName);
+    setEnteredName('');
+  };
+
   return (
-    <form>
-      <div className='form-control'>
-        <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
+    <form onSubmit={formSubmissionHandler}>
+      <div className="form-control">
+        <label htmlFor="name">Your Name</label>
+        <input
+          type="text"
+          id="name"
+          onChange={nameInputChangeHanlder}
+          value={enteredName}
+        />
       </div>
       <div className="form-actions">
         <button>Submit</button>
