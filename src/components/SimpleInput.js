439b57bb1da7e54de3d89277776bbdfd197eef2b
Ryan Cabanas <ryangithub777@gmail.com>
2022-07-23 23:48:06 -0400

    After Lesson 206, Assignment 5: Add an email input too and validate it.
    - This was pretty easy because you're just replicating all the work done for
      the "name" input.  The only thing different, really, was how you validate the
      "email" input.

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,18 +1,31 @@
 import { useState } from 'react';
 
 const SimpleInput = (props) => {
+  // Name states
   const [enteredName, setEnteredName] = useState('');
   const [enteredNameTouched, setEnteredNameTouched] = useState(false);
 
+  // Email states
+  const [enteredEmail, setEnteredEmail] = useState('');
+  const [enteredEmailTouched, setEnteredEmailTouched] = useState(false);
+
+  // Name validation
   const enteredNameIsValid = enteredName.trim() !== '';
   const nameInputIsInvalid = !enteredNameIsValid && enteredNameTouched;
 
+  // Email validation
+  const emailRegEx = /^[\w.]+[+]*[\w.]*@\w+\.\w+$/;
+  const enteredEmailIsValid = emailRegEx.test(enteredEmail.trim());
+  const emailInputIsInvalid = !enteredEmailIsValid && enteredEmailTouched;
+
+  // Form validation tracking
   let formIsValid = false;
 
-  if (enteredNameIsValid) {
+  if (enteredNameIsValid && enteredEmailIsValid) {
     formIsValid = true;
   }
 
+  // Name funcitons
   const nameInputChangeHandler = (event) => {
     setEnteredName(event.target.value);
   };
@@ -21,20 +34,42 @@ const SimpleInput = (props) => {
     setEnteredNameTouched(true);
   };
 
+  // Email functions
+  const emailInputChangeHandler = (event) => {
+    setEnteredEmail(event.target.value);
+  };
+
+  const emailInputBlurHandler = (event) => {
+    setEnteredEmailTouched(true);
+  };
+
   const formSubmissionHandler = (event) => {
     event.preventDefault();
-    setEnteredNameTouched(true);
 
-    if (!enteredNameIsValid) {
+    setEnteredNameTouched(true);
+    setEnteredEmailTouched(true);
+
+    if (!enteredNameIsValid || !enteredEmailIsValid) {
       return;
     }
 
-    console.log(enteredName.trim());
+    console.log(`Name: ${enteredName.trim()}`);
+    console.log(`Email: ${enteredEmail.trim()}`);
+
     setEnteredName('');
+    setEnteredEmail('');
+
     setEnteredNameTouched(false);
+    setEnteredEmailTouched(false);
   };
 
+  // Name CSS class
   const nameInputClasses = nameInputIsInvalid
+    ? 'form-control invalid'
+    : 'form-control';
+
+  // Email CSS class
+  const emailInputClasses = emailInputIsInvalid
     ? 'form-control invalid'
     : 'form-control';
 
@@ -54,6 +89,17 @@ const SimpleInput = (props) => {
             Name must not be empty, or all whitespace.
           </p>
         )}
+      </div>
+      <div className={emailInputClasses}>
+        <label htmlFor="email">Your Email</label>
+        <input
+          type="text"
+          id="email"
+          onChange={emailInputChangeHandler}
+          value={enteredEmail}
+          onBlur={emailInputBlurHandler}
+        />
+        {emailInputIsInvalid && <p className="error-text">Email is invalid.</p>}
       </div>
       <div className="form-actions">
         <button disabled={!formIsValid}>Submit</button>
