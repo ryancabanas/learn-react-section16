9174d8def016571ea5c57c3a242aaaeee9910d8e
Ryan Cabanas <ryangithub777@gmail.com>
2022-07-24 23:57:12 -0400

    Squashed commit of the following:
    commit 2d23da7862827c0642e2ba373cf2d46925090e46
    Author: Ryan Cabanas <ryangithub777@gmail.com>
    Date:   Sun Jul 24 23:56:17 2022 -0400
    
        End of Lesson 209: Implement the `useInput2` custom hook for the "Email" input
        field.
    
    commit 36261960818b71706a3104ed6e6e74cfad5e26c8
    Author: Ryan Cabanas <ryangithub777@gmail.com>
    Date:   Sun Jul 24 23:51:11 2022 -0400
    
        Lesson 209: Implement the custom hook for the "Last Name" field.
    
    commit cc71e6d697b6114b08ffc2c687ca437ae72d7676
    Author: Ryan Cabanas <ryangithub777@gmail.com>
    Date:   Sun Jul 24 23:47:57 2022 -0400
    
        Lesson 209: Create my version of the `useInput` custom hook and implement it
        into the `BasicForm` component for the "First Name" field only for now to make
        sure it works as expected, which it does.
    
    commit 396841d73fa919ffa456e8fdd7619cb8c961d35d
    Author: Ryan Cabanas <ryangithub777@gmail.com>
    Date:   Sun Jul 24 23:03:22 2022 -0400
    
        Lesson 209: Swap out the `SimpleInput` component for the `BasicForm` component
        in the `App` component and start cleaning up the input elements.

--- src/components/BasicForm.js
+++ src/components/BasicForm.js
@@ -1,22 +1,105 @@
+import useInput2 from '../hooks/use-input2';
+
 const BasicForm = (props) => {
+  const {
+    value: firstName,
+    valueIsValid: firstNameIsValid,
+    inputHasError: firstNameHasError,
+    changeHandler: firstNameChangeHandler,
+    blurHandler: firstNameBlurHandler,
+    reset: resetFirstName,
+  } = useInput2((value) => value.trim() !== '');
+
+  const {
+    value: lastName,
+    valueIsValid: lastNameIsValid,
+    inputHasError: lastNameHasError,
+    changeHandler: lastNameChangeHandler,
+    blurHandler: lastNameBlurHandler,
+    reset: resetLastName,
+  } = useInput2((value) => value.trim() !== '');
+
+  const {
+    value: email,
+    valueIsValid: emailIsValid,
+    inputHasError: emailHasError,
+    changeHandler: emailChangeHandler,
+    blurHandler: emailBlurHandler,
+    reset: resetEmail,
+  } = useInput2((value) => {
+    const emailRegEx = /^[\w.]+[+]*[\w.]*@\w+\.\w+$/;
+    return emailRegEx.test(value.trim());
+  });
+
+  const formSubmissionHandler = (event) => {
+    event.preventDefault();
+
+    console.log(`First Name: ${firstName}`);
+    console.log(`Last Name: ${lastName}`);
+    console.log(`Email: ${email}`);
+
+    resetFirstName();
+    resetLastName();
+    resetEmail();
+  };
+
+  const formIsValid = firstNameIsValid && lastNameIsValid && emailIsValid;
+
+  const firstNameClasses = firstNameHasError
+    ? 'form-control invalid'
+    : 'form-control';
+
+  const lastNameClasses = lastNameHasError
+    ? 'form-control invalid'
+    : 'form-control';
+
+  const emailClasses = emailHasError ? 'form-control invalid' : 'form-control';
+
   return (
-    <form>
-      <div className='control-group'>
-        <div className='form-control'>
-          <label htmlFor='name'>First Name</label>
-          <input type='text' id='name' />
+    <form onSubmit={formSubmissionHandler}>
+      <div className="control-group">
+        <div className={firstNameClasses}>
+          <label htmlFor="first-name">First Name</label>
+          <input
+            type="text"
+            id="first-name"
+            onChange={firstNameChangeHandler}
+            onBlur={firstNameBlurHandler}
+            value={firstName}
+          />
+          {firstNameHasError && (
+            <p className="error-text">Cannot be empty, or all whitespace</p>
+          )}
         </div>
-        <div className='form-control'>
-          <label htmlFor='name'>Last Name</label>
-          <input type='text' id='name' />
+        <div className={lastNameClasses}>
+          <label htmlFor="last-name">Last Name</label>
+          <input
+            type="text"
+            id="last-name"
+            onChange={lastNameChangeHandler}
+            onBlur={lastNameBlurHandler}
+            value={lastName}
+          />
+          {lastNameHasError && (
+            <p className="error-text">Cannot be empty, or all whitespace</p>
+          )}
         </div>
       </div>
-      <div className='form-control'>
-        <label htmlFor='name'>E-Mail Address</label>
-        <input type='text' id='name' />
+      <div className={emailClasses}>
+        <label htmlFor="email">E-Mail Address</label>
+        <input
+          type="email"
+          id="email"
+          onChange={emailChangeHandler}
+          onBlur={emailBlurHandler}
+          value={email}
+        />
+        {emailHasError && (
+          <p className="error-text">Must be valid email address</p>
+        )}
       </div>
-      <div className='form-actions'>
-        <button>Submit</button>
+      <div className="form-actions">
+        <button disabled={!formIsValid}>Submit</button>
       </div>
     </form>
   );
